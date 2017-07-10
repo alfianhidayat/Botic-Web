3af42f2fcba4e1c68692785556739efff826db24
alfianh <alfian08.ah@gmail.com>
2017-07-11 05:53:56 +0700

    api checkin

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,7 @@ Route::resource('finance', 'ApiFinanceController');
 Route::resource('health', 'ApiHealthController');
 Route::resource('leisure', 'ApiLeisureController');
 Route::resource('booking', 'ApiBookingController');
+Route::resource('checkin', 'ApiCheckInController');
 Route::get('listasset', 'ApiBookingController@getListAsset');
 Route::get('listidentity', 'ApiBookingController@getListIdentity');
 Route::post('login', 'ApiUserController@login');
