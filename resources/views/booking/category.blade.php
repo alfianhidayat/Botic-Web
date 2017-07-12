6e3ce9f4815e62ce3ccdc9cea8d49cb7e54a6498
findco_team <findcoteam@gmail.com>
2017-07-12 21:30:33 +0700

    update tanggal

--- resources/views/booking/category.blade.php
+++ resources/views/booking/category.blade.php
@@ -102,12 +102,12 @@
                     <div class="modal-body">
                         <div class="form-group">
                             <label>Dari</label>
-                            <input type="date" name="from" class="form-control" placeholder="Dari"
+                            <input id="dateField" type="date" min="2014-01-01" disabled name="from" class="form-control" placeholder="Dari"
                                    required/>
                         </div>
                         <div class="form-group">
                             <label>Sampai</label>
-                            <input type="date" name="until" class="form-control" placeholder="Sampai"
+                            <input id="dateField" type="date" min="2014-01-01" disabled name="until" class="form-control" placeholder="Sampai"
                                    required/>
                         </div>
                     </div>
@@ -125,6 +125,23 @@
     </div>
     {{--END MODAL EXPORT--}}
     <script>
+
+        var input = document.getElementById("dateField");
+        var today = new Date();
+        var day = today.getDate();
+
+        // Set month to string to add leading 0
+        var mon = new String(today.getMonth()+1); //January is 0!
+        var yr = today.getFullYear();
+
+        if(mon.length < 2) { mon = "0" + mon; }
+
+        var date = new String( yr + '-' + mon + '-' + day );
+
+        input.disabled = false;
+        input.setAttribute('min', date);
+
+
         function hapus() {
             swal({
                 title: 'Apakah anda yakin?',
--- resources/views/booking/insert.blade.php
+++ resources/views/booking/insert.blade.php
@@ -72,7 +72,7 @@
 
                                         <div class="form-group">
                                             <label>Tanggal</label>
-                                            <input type="date" name="date" class="form-control" placeholder="Telepon"
+                                            <input id="dateField" type="date" min="2014-01-01" disabled name="from" class="form-control" placeholder="Tanggal"
                                                    required/>
                                         </div>
                                         @if($data->id == 29)
@@ -116,36 +116,19 @@
         </div>
     </div>
     <script>
-        //        function tambah() {
-        //            swal({
-        //                title: 'Are you sure?',
-        //                text: "You won't be able to revert this!",
-        //                type: 'warning',
-        //                showCancelButton: true,
-        //                confirmButtonColor: '#3085d6',
-        //                cancelButtonColor: '#d33',
-        //                confirmButtonText: 'Yes, delete it!',
-        //                cancelButtonText: 'No, cancel!',
-        //                confirmButtonClass: 'btn btn-success',
-        //                cancelButtonClass: 'btn btn-danger',
-        //                buttonsStyling: false
-        //            }).then(function () {
-        //                swal(
-        //                    'Deleted!',
-        //                    'Your file has been deleted.',
-        //                    'success'
-        //                )
-        //            }, function (dismiss) {
-        //                // dismiss can be 'cancel', 'overlay',
-        //                // 'close', and 'timer'
-        //                if (dismiss === 'cancel') {
-        //                    swal(
-        //                        'Cancelled',
-        //                        'Your imaginary file is safe :)',
-        //                        'error'
-        //                    )
-        //                }
-        //            });
-        //        }
+        var input = document.getElementById("dateField");
+        var today = new Date();
+        var day = today.getDate();
+
+        // Set month to string to add leading 0
+        var mon = new String(today.getMonth()+1); //January is 0!
+        var yr = today.getFullYear();
+
+        if(mon.length < 2) { mon = "0" + mon; }
+
+        var date = new String( yr + '-' + mon + '-' + day );
+
+        input.disabled = false;
+        input.setAttribute('min', date);
     </script>
 @endsection
